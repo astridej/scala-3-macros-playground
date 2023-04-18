53ac219043a4be51ceeb83adb9137f0dc786da35
Astrid Jahn <astrid.jahn@mobimeo.com>
2023-04-18 14:49:50 +0200

    Remove unnecessary brackets

--- src/main/scala/testing/Arithmetic.scala
+++ src/main/scala/testing/Arithmetic.scala
@@ -5,7 +5,7 @@ import scala.quoted.*
 def plusStaticCode(x: Expr[Int], y: Expr[Int])(using Quotes): Expr[Int] =
   Expr(x.valueOrAbort + y.valueOrAbort)
 
-def plusDynamicCode(x: Expr[Int], y: Expr[Int])(using Quotes): Expr[Int] = '{ ($x + $y) }
+def plusDynamicCode(x: Expr[Int], y: Expr[Int])(using Quotes): Expr[Int] = '{ $x + $y }
 
 def compilerFibonacciCode(x: Expr[Int])(using Quotes): Expr[Int] = {
   val value = x.valueOrAbort
